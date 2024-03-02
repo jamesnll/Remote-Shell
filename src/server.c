e673ac46191b8c4b9573c4dcb025b344597cd5c7
jamesnll <langillejames11@gmail.com>
2024-03-01 17:17:01 -0800

    Added client code into server

--- src/server.c
+++ src/server.c
@@ -1,8 +1,87 @@
+#include "../include/arguments.h"
+#include <p101_c/p101_string.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Hello world\n");
-    return EXIT_SUCCESS;
+    int                ret_val;
+    struct p101_error *error;
+    struct p101_env   *env;
+    struct arguments   arguments;
+    struct context     context;
+
+    error = p101_error_create(false);
+
+    if(error == NULL)
+    {
+        ret_val = EXIT_FAILURE;
+        goto done;
+    }
+
+    env = p101_env_create(error, true, NULL);
+    if(p101_error_has_error(error))
+    {
+        ret_val = EXIT_FAILURE;
+        goto free_error;
+    }
+
+    p101_memset(env, &arguments, 0, sizeof(arguments));    // Set memory of arguments to 0
+    p101_memset(env, &context, 0, sizeof(context));        // Set memory of context to 0
+    context.arguments       = &arguments;
+    context.arguments->argc = argc;
+    context.arguments->argv = argv;
+
+    parse_arguments(env, error, &context);
+    check_arguments(env, error, &context);
+    parse_in_port_t(env, error, &context);
+    if(p101_error_has_error(error))
+    {
+        ret_val = EXIT_FAILURE;
+        goto free_env;
+    }
+    convert_address(env, error, &context);
+    if(p101_error_has_error(error))
+    {
+        ret_val = EXIT_FAILURE;
+        goto free_env;
+    }
+
+    ret_val = EXIT_SUCCESS;
+
+free_env:
+    free(context.exit_message);
+    free(env);
+
+free_error:
+    if(p101_error_has_error(error))
+    {
+        fprintf(stderr, "Error: %s\n", p101_error_get_message(error));
+    }
+    p101_error_reset(error);
+    free(error);
+
+done:
+    printf("Exit code: %d\n", ret_val);
+    return ret_val;
 }
+
+/*
+ * Parent Process
+ *      | // fork here before setting up server
+ *      |
+ *      |
+ * Process Manager
+ *      |
+ *      |
+ * Child processes
+ *
+ * use domain sockets to transfer fds from one process to another, d'arcy might be lying ):
+ * create the domain socket, then fork (check out socket pair example)
+ *
+ * call socketpair()
+ * then call fork() // child process has access to the other side of the socketpair
+ * from that child process call fork()
+ *
+ */
