dc4f9fb4e77ce8834e920fe07e67bba0ab546eb7
jamesnll <langillejames11@gmail.com>
2024-03-02 13:32:14 -0800

    Removed commented out code

--- src/server.c
+++ src/server.c
@@ -7,11 +7,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// static volatile sig_atomic_t exit_flag = 0;    // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
-
-// void setup_signal_handler(void);
-// void sigint_handler(int signum);
-
 int main(int argc, char *argv[])
 {
     int                ret_val;
@@ -119,38 +114,3 @@ done:
     printf("Exit code: %d\n", ret_val);
     return ret_val;
 }
-
-// void setup_signal_handler(void)
-//{
-//     struct sigaction sa;
-//
-//     memset(&sa, 0, sizeof(sa));
-//
-// #if defined(__clang__)
-//     #pragma clang diagnostic push
-//     #pragma clang diagnostic ignored "-Wdisabled-macro-expansion"
-// #endif
-//     sa.sa_handler = sigint_handler;
-// #if defined(__clang__)
-//     #pragma clang diagnostic pop
-// #endif
-//
-//     sigemptyset(&sa.sa_mask);
-//     sa.sa_flags = 0;
-//
-//     if(sigaction(SIGINT, &sa, NULL) == -1)
-//     {
-//         perror("sigaction");
-//         exit(EXIT_FAILURE);
-//     }
-// }
-//
-// #pragma GCC diagnostic push
-// #pragma GCC diagnostic ignored "-Wunused-parameter"
-//
-// void sigint_handler(int signum)
-//{
-//     exit_flag = 1;
-// }
-//
-// #pragma GCC diagnostic pop
